61f6d25683cb33e0783a52862ebb2525d6630112
homer0 <me@homer0.dev>
2021-08-22 02:49:03 -0300

    chore: add test endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 
 import fastify from 'fastify';
+import * as sharp from 'sharp';
 import env from './env';
 
 // -- Constants
@@ -11,9 +12,29 @@ const PORT = env.port || DEFAULT_PORT;
 
 const app = fastify({ logger: true });
 
-// ---- Health
+// ---- Endpoints
 
 app.get('/health', async (req, res) => res.send({ healthy: true }));
+
+app.get('/test', async (req, res) => {
+  const watermark = Buffer.from(`<svg>
+    <rect x="0" y="0" width="500" height="100" fill="#000" />
+    <text x="10" y="76" font-size="74" fill="#fff" font-family="monospace">Test</text>
+  </svg>`);
+  const image = await sharp({
+    create: {
+      width: 500,
+      height: 500,
+      channels: 4,
+      background: { r: 90, g: 189, b: 28, alpha: 1 },
+    },
+  })
+    .composite([{ input: watermark, gravity: 'southeast' }])
+    .png()
+    .toBuffer();
+
+  res.header('Content-Type', 'image/png').send(image);
+});
 
 // -- Run Fastify
 
