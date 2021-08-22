f9fe8ee9527752951bcaf0f4fab91139c78e44a1
homer0 <me@homer0.dev>
2021-08-22 03:56:12 -0300

    chore: re-enable ESLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,10 +18,6 @@ module.exports = {
     'node/no-unsupported-features/es-syntax': ['error', { ignores: ['modules'] }],
     'import/extensions': 'off',
     'import/prefer-default-export': 'off',
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
   },
   settings: {
     node: {
